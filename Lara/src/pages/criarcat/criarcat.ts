d33de39abeb40673738b827c09d13f6d13e256ba
Gabriela Miyajima Christianini <ra1657106@CTIBAURU.FEB.UNESP.BR>
2018-09-24 08:48:34 -0300

    Session na criar categoria

--- Lara/src/pages/criarcat/criarcat.ts
+++ Lara/src/pages/criarcat/criarcat.ts
@@ -33,6 +33,7 @@ export class CriarcatPage implements OnInit //implementar OnInit
     errorImagem= false;
 	// variável que será o src da imagem mostrada (mickey putaço)
 	imagem:string = "https://img.olx.com.br/images/86/864708037631038.jpg";
+	usuario: Usuario;
 	categoriasG:any;
 	public base64Image: string;
 	
@@ -52,7 +53,7 @@ export class CriarcatPage implements OnInit //implementar OnInit
   		 this.criarCategoriaForm = formBuilder.group(
       {
         nomeCategoria: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z]+$')])],
-		txtimg: ['',Validators.required],
+		// txtimg: ['',Validators.required],
 		
       });
   }
@@ -93,18 +94,52 @@ converte(date){
     }
     return retorno;
   }
+  currentImage = null;
+	captureImage() {
+	  const options: CameraOptions = {
+		sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+		destinationType: this.camera.DestinationType.DATA_URL,
+		quality : 25,
+		targetWidth: 512,
+		targetHeight: 512,
+		encodingType: 1,
+		correctOrientation: true
+	  }
+	  this.camera.getPicture(options).then((imageData) => {
+		  this.base64Image = "data:image/jpeg;base64," + imageData;    }, (err) => {
+			  console.log(err);
+		// Handle error
+		console.log('Image error: ', err);
+	  });
+	}
+	takePicture(){
+	  this.camera.getPicture({
+		  destinationType: this.camera.DestinationType.DATA_URL,
+		  quality: 25,
+		  targetWidth: 512,
+		  targetHeight: 512,
+		  encodingType: 1
+	  }).then((imageData) => {
+		// imageData is a base64 encoded string
+		  this.base64Image = "data:image/jpeg;base64," + imageData;
+	  }, (err) => {
+		  console.log(err);
+	  });
+	}
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad InserecodPage');
+	  }
+
 	criar()
     {
 		let nomes = [];
 		for(let n = 0; n < this.categoriasG.length; n++)
 			nomes.push(this.categoriasG[n]['nome_categoria'].toLowerCase());
 
-
-        let {nomeCategoria,txtimg} = this.criarCategoriaForm.controls;
+        let {nomeCategoria/*,txtimg*/} = this.criarCategoriaForm.controls;
 
         if (!this.criarCategoriaForm.valid)
         {
-
 			if (!nomeCategoria.valid)
 			{
 				if(	nomeCategoria.value== null || nomeCategoria.value=="")
@@ -120,17 +155,12 @@ converte(date){
 				
 				
 			}
-			if (!txtimg.valid)
-			{
-				this.errorImagem = true;
-				this.messageImagem = "Campo obrigatorio";
-			}
-	
+			
 		}
 		else
 		{
 			this.messagenomeCategoria = "";
-			this.messageImagem= "";
+			//this.messageImagem= "";
 
 			if(nomes.indexOf(nomeCategoria.value.toLowerCase()) != -1){
 				this.errornomeCategoria = true;
@@ -151,15 +181,22 @@ converte(date){
 				alert("Selecione no minimo um checkbox!");
 				return;
 			}
+			// if (!txtimg.valid)
+			// {
+			// 	this.errorImagem = true;
+			// 	this.messageImagem = "Campo obrigatorio";
+			// }
+	
 			let palavras = [];
 			for(let a = 0; a < ckbs.length; a++){
 				let ckb = <HTMLInputElement> ckbs[a];
 				if(ckb.checked)
 					palavras.push(ckb.id.substring(3));
 			}
+	
 			let objeto = {
 				nome_categoria:nomeCategoria.value,
-				id_usuario:null			
+				id_usuario: this.usuario.id_usuario			
 				
 			};
 
@@ -169,16 +206,16 @@ converte(date){
 				alert(JSON.stringify(error) + " erro na inclusão de categorias. Favor contactar os desenvolvedores");
 			  	return;
 			});
-
 			setTimeout(() => {
 				this.http.get('http://inclusio.engynios.com/api/read/nome/categoria.php', {nome_categoria: '"'+nomeCategoria.value+'"'}, {headers: { 'Content-Type': 'application/json' }})
 				.then(data => {
 					let dados = this.converte(data.data);
 					for(let a = 0; a < palavras.length; a++){
+	
 						let ad = {
 							id_categoria: dados[dados.length-1]['id_categoria'],//pegar a ultima categoria
 							id_palavra: palavras[a],
-							id_usuario: null// usuario da session
+							id_usuario: this.usuario.id_usuario
 						}
 
 
@@ -194,51 +231,29 @@ converte(date){
 					return;
 				});
 			}, 5000);
-
 		}
 	}
 
 	ngOnInit() {
-		this.carregaCategorias();
+		this.getSession();
+		setTimeout(() => {
+			this.carregaCategorias();
+		}, 3000);
 	}
-
-	currentImage = null;
-	captureImage() {
-	  const options: CameraOptions = {
-		sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-		destinationType: this.camera.DestinationType.DATA_URL,
-		quality : 100,
-		targetWidth: 1000,
-		targetHeight: 1000,
-		correctOrientation: true
-	  }
-	  this.camera.getPicture(options).then((imageData) => {
-		  this.base64Image = "data:image/jpeg;base64," + imageData;    }, (err) => {
-			  console.log(err);
-		// Handle error
-		console.log('Image error: ', err);
-	  });
-	}
-	takePicture(){
-	  this.camera.getPicture({
-		  destinationType: this.camera.DestinationType.DATA_URL,
-		  targetWidth: 1000,
-		  targetHeight: 1000
-	  }).then((imageData) => {
-		// imageData is a base64 encoded string
-		  this.base64Image = "data:image/jpeg;base64," + imageData;
-	  }, (err) => {
-		  console.log(err);
-	  });
-	}
-	ionViewDidLoad() {
-		console.log('ionViewDidLoad InserecodPage');
-	  }
 
 	// muda a imagem com base no input do usuário
 	mudaImg(){
 		// define a variável "imagem" como o texto colocado no input
 		this.imagem = document.getElementById("txtimg").nodeValue;
+	}
+
+	getSession(){
+		this.session_login.get().then(
+			res =>
+			{
+				this.usuario = res;	
+			}
+		);//session			
 	}
 
 	// limpa todos os campos
@@ -260,93 +275,13 @@ converte(date){
 			}
 		}
 	
-		// se existirem elementos dentro da table palavras, fica no while
-		// while(palavras.childElementCount > 0){
-		// 	// remove o último elemento de palavras
-		// 	palavras.removeChild(palavras.lastChild);
-		// }
-	
-	// carregaCategorias(){
-	// 	let objeto = {
-	// 		id_usuario: null
-	// 	};
-	// 	let path = 'http://inclusio.engynios.com/api/read/id_usuario/categoria.php';
-	// 	this.http.get(path, objeto, {}).then(data =>{
-	// 		let dados = this.converte(data.data);
-	// 		// alert(JSON.stringify(data.data));
-	// 		// alert(JSON.stringify(dados));
-	// 		let div = document.getElementById('div_categorias');
-	// 		// alert(dados[0].id_categoria);	
-
-	// 		for(let a = 0; a < dados.length; a++){
-	// 			let ion = document.createElement('ion-item');
-	// 			// let icon = document.getElementById('hiddenIcon');
-	// 			// let icon = document.createElement('ion-icon');
-	// 			// icon.setAttribute('[name]', 'arrow-dropright')
-	// 			let icon = <HTMLImageElement> document.createElement('img');
-	// 			icon.id = 'seta'+dados[a]['id_categoria'];
-	// 			icon.src = 'assets/imgs/arrow-right.png'; 
-	// 			icon.className = 'seta-direita';
-	// 			let chama = function(){
-	// 				this.mostraPalavras(dados[a].id_categoria);
-	// 			}
-	// 			icon.onclick = chama;
-	// 			let ckb = <HTMLInputElement> document.createElement('input');
-	// 			ckb.type = "checkbox";
-	// 			ckb.id = "ckb"+dados[a]['id_categoria'];
-	// 			ckb.className = 'checkbox';
-	// 			let cat = document.createElement('p');
-	// 			cat.innerText = dados[a]['nome_categoria'];
-	// 			cat.appendChild(document.createElement('br'));
-	// 			ion.appendChild(icon);
-	// 			ion.appendChild(ckb);
-	// 			ion.appendChild(cat);
-	// 			div.appendChild(ion);
-	// 		}
-			
-	// 		}
-	// 	).catch(error => {
-	// 		  alert(JSON.stringify(error));
-	// 	});
-			
-			
-		
-	// 	/*objeto.id_usuario = 3;
-	// 	this.http.get(path, objeto, {}).then(data =>{
-	// 		data = data.data;
-	// 		let div = document.getElementById('div_categorias');
-			
-	// 		div.name
-			
-	// 		for(let a = 0; a < data.length; a++){
-	// 			let ion = document.createElement('ion-item');
-	// 			let icon = document.createElement('ion-icon');
-	// 			icon.name = "seta()";
-	// 			icon.click = mostraPalavras;
-	// 			icon.id = "seta"+a;
-	// 			let ckb = document.createElement('input');
-	// 			ckb.type = "checkbox";
-	// 			ckb.id = "ckb"+a;
-	// 			ckb.name = dados.id_categoria;
-	// 			let cat = document.createElement(p);
-	// 			cat.innerText = dados.nome_categoria;
-	// 			cat.appendChild(document.createElement('br'));
-	// 			ion.appendChild(icon);
-	// 			ion.appendChild(ckb);
-	// 			ion.appendChild(cat);
-	// 			div.appendChild(ion);
-	// 		}
-	// 		}
-	// 	).catch(error => {
-	// 		  alert(JSON.stringify(error));
-	// 	});*/
-	// }
 
 	carregaCategorias(){
+	
 		let objeto = {
-			id_usuario: null
+			id_usuario: this.usuario.id_usuario
 		};
-		let path = 'http://inclusio.engynios.com/api/read/id_usuario/categoria.php';
+		let path = 'http://inclusio.engynios.com/api/read/id_usuario/categoria-null.php';
 		this.http.get(path, objeto, {}).then(data =>{
 			let dados = this.converte(data.data);
 			this.categoriasG = dados;
