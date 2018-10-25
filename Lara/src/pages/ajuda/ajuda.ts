69e53485b35a78d4f388ed4704de7c36a384b37d
Gabriela Miyajima Christianini <ra1657106@CTIBAURU.FEB.UNESP.BR>
2018-10-25 09:50:57 -0200

    atualizei a ajuda

--- Lara/src/pages/ajuda/ajuda.ts
+++ Lara/src/pages/ajuda/ajuda.ts
@@ -15,11 +15,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AjudaPage {
 
+  mostra_botoes: boolean = true;  //começa mostrando o botão para ir para o proximo 
+  mostra_slides: boolean = false; //enquanto isso, os slides não aparecem
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AjudaPage');
+  }
+  ajuda_slides()
+  {
+
+      //depois esconde-se a div dos botoes 
+			this.mostra_botoes = !this.mostra_botoes;
+      //ele pega o valor true (ou seja mostrando) e troca para false (não aparece)
+      
+      //e chama a div onde estão os slides
+      this.mostra_slides = !this.mostra_slides;
   }
 
 }
